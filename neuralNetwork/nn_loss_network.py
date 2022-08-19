9820bbc2ca1ec4ce04ad4da0efd0bbf05bf8489e
Niii-sh <1159417019@qq.com>
2022-08-19 23:18:06 +0800

    完成损失函数的学习

---
+++ neuralNetwork/nn_loss_network.py
@@ -0,0 +1,44 @@
+import torch
+import torchvision
+from torch import nn
+from torch.nn import Conv2d, MaxPool2d, Flatten, Linear, Sequential
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+
+dataset = torchvision.datasets.CIFAR10("../data", train=False, transform=torchvision.transforms.ToTensor(),
+                                       download=True)
+
+dataLoader = DataLoader(dataset, batch_size=1)
+
+
+class Tudui(nn.Module):
+    def __init__(self):
+        super(Tudui, self).__init__()
+        # 使用 Sequential 自定义神经网络的执行顺序
+        self.model1 = Sequential(
+            Conv2d(3, 32, 5, padding=2),
+            MaxPool2d(2),
+            Conv2d(32, 32, 5, padding=2),
+            MaxPool2d(2),
+            Conv2d(32, 64, 5, padding=2),
+            MaxPool2d(2),
+            Flatten(),
+            Linear(1024, 64),
+            Linear(64, 10)
+        )
+
+    def forward(self, x):
+        x = self.model1(x)
+        return x
+
+
+# 交叉熵
+loss = nn.CrossEntropyLoss()
+
+tudui = Tudui()
+
+for data in dataLoader:
+    imgs, targets = data
+    outputs = tudui(imgs)
+    result_loss = loss(outputs,targets)
+    print(result_loss)
